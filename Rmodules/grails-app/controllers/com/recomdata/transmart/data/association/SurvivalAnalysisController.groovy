546629964ff2128ff5f9145a2470dd0264537c97
Zach Wright <zwright@umich.edu>
2024-08-01 12:25:45 -0400

    Update SurvivalAnalysisController.groovy

--- Rmodules/grails-app/controllers/com/recomdata/transmart/data/association/SurvivalAnalysisController.groovy
+++ Rmodules/grails-app/controllers/com/recomdata/transmart/data/association/SurvivalAnalysisController.groovy
@@ -213,7 +213,7 @@ class SurvivalAnalysisController {
 	bufHeader
     }
 
-    private Closure parseLegendTable = { String legendInStr ->
+    private String parseLegendTable = { String legendInStr ->
 
 	StringBuilder sb = new StringBuilder()
 
