773e41b1ad5a55e023a21ad7df2b740a9125be55
Zach Wright <zwright@umich.edu>
2026-06-09 15:06:28 -0400

    Update dataAssociation.js

--- Rmodules/grails-app/assets/javascripts/dataAssociation.js
+++ Rmodules/grails-app/assets/javascripts/dataAssociation.js
@@ -246,7 +246,7 @@ function loadPluginView(){
         console.warn('loadPluginView: waiting for', loadViewName, '...');
         setTimeout(function() { loadPluginView(); }, 50);
     }
-
+}
 //This function fires when an item is dropped onto one of the independent/dependent variable DIVs in the data association tool.
 function dropOntoVariableSelection(source, e, data)
 {
